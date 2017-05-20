cd7872939f74eb8c1c05cc0071dc6c92f8e5d294
Samuel Fahlberg <sam.fahlberg@gmail.com>
2017-05-20 16:17:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Evernote Manager App
+==============================================
+
+Setup
+-----
+- Fill your token, API Consumer Key and Consumer Secret in config.json
+- `yarn install`
+- `node app.js`
---
+++ app.js
@@ -0,0 +1,57 @@
+var Evernote = require('evernote');
+var config = require('./config.json');
+var without = require('lodash/without');
+
+
+var zeroTodayGuid;
+var oneTomorrowGuid;
+var oneTomorrowString = '1-tomorrow';
+var zeroTodayString = '0-today';
+
+var client = new Evernote.Client({
+  token: config.TOKEN,
+  sandbox: config.SANDBOX,
+  china: config.CHINA
+});
+
+var noteStore = client.getNoteStore();
+
+var filter = new Evernote.NoteStore.NoteFilter({
+  words: 'tag:' + oneTomorrowString,
+  ascending: true
+});
+
+var spec = new Evernote.NoteStore.NotesMetadataResultSpec({
+  includeTitle: true,
+  includeTagGuids: true,
+});
+
+noteStore.listTags().then(function(tags) {
+  tags.forEach(function(tag) {
+    if (tag.name === zeroTodayString) {
+      zeroTodayGuid = tag.guid
+    } else if (tag.name === oneTomorrowString) {
+      oneTomorrowGuid = tag.guid
+    }
+  })
+}, function(error) {
+  console.log(error);
+});
+
+noteStore.findNotesMetadata(filter, 0, 500, spec).then(function(tags) {
+  tags.notes.forEach(function(note) {
+    newTagGuids = without(note.tagGuids, oneTomorrowGuid);
+    newTagGuids.push(zeroTodayGuid);
+    noteStore.updateNote({
+      guid: note.guid,
+      title: note.title,
+      tagGuids: newTagGuids
+    }).then(function(data) {
+      console.log('successfully updated');
+    }, function(error) {
+      console.log(error);
+    })
+  })
+}, function(error) {
+  console.log(error);
+});
